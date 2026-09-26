edd60864ef43a1a86fe2f564355ab0d829bdd892
Syahprabu <syahprabuarhen@gmail.com>
2026-09-26 22:36:50 +0700

    DP1

--- DP1.py
+++ DP1.py
@@ -1,60 +1,42 @@
-"""
-Tugas Praktikum Dasar Pemrograman 1
-Mini SIAKAD (Rapor Mahasiswa)
-"""
+nama = input("Masukkan nama mahasiswa: ")
+nilai = []
 
-# 1. INPUT DATA MAHASISWA
+#looping input nilai
+for i in range (3) :
+    nilai_input = float(input(f"Masukkan nilai ke - {i + 1} : "))
+    nilai.append(nilai_input)
 
-nama_mahasiswa = input("Masukkan nama mahasiswa: ")
+kehadiran = float(input("Masukkan persentase kehadiran (%) : "))
 
-# List untuk menyimpan tiga nilai tugas
-nilai_tugas = []
-for i in range(1, 4):
-    nilai = float(input(f"Masukkan nilai tugas ke-{i}: "))
-    nilai_tugas.append(nilai)
+total = 0
+for i in nilai :
+    total += i
+    print(f"total : {total}")
+    rata = total / len(nilai)
 
-persentase_kehadiran = float(input("Masukkan persentase kehadiran (%): "))
+matkul = input("Masukkan mata kuliah: ")
+semester = int(input("Masukkan semester: "))
 
-# 2. TOTAL & RATA-RATA NILAI TUGAS (for loop)
+matkul_semester = (matkul,semester)
 
-total_nilai = 0
-for nilai in nilai_tugas:
-    total_nilai += nilai
+bonus = rata * 0.05
 
-rata_rata_tugas = total_nilai / len(nilai_tugas)
+nilai_akhir = rata + bonus
 
-# 3. TUPLE: NAMA MATA KULIAH & SEMESTER
+if nilai_akhir > 100 :
+    nilai_akhir =100
 
-info_mk = ("Dasar Pemrograman", 1)  
+if nilai_akhir >= 70 and kehadiran >= 75:
+    status_lulus = True
+else :
+    status_lulus= False
 
-# 4. BONUS & NILAI AKHIR
-
-bonus = 0.05 * rata_rata_tugas          
-nilai_akhir = rata_rata_tugas + bonus
-
-
-# 5. STATUS KELULUSAN (boolean)
-
-lulus = nilai_akhir >= 70 and persentase_kehadiran >= 75
-
-# 6. DICTIONARY RINGKASAN RAPOR
-
-rapor = {
-    "nama": nama_mahasiswa,
-    "mata_kuliah": info_mk[0],
-    "semester": info_mk[1],
-    "nilai_tugas": nilai_tugas,
-    "total_nilai": total_nilai,
-    "rata_rata_tugas": round(rata_rata_tugas, 2),
-    "bonus": round(bonus, 2),
-    "nilai_akhir": round(nilai_akhir, 2),
-    "kehadiran (%)": persentase_kehadiran,
-    "lulus": lulus,
+ringkasan ={
+    "Nama" : nama , 
+    "Mata Kuliah" : matkul_semester,
+    "Persentase Kehadiran" : f"{kehadiran:.0f} %",
+    "Nilai Akhir" : nilai_akhir,
+    "Lulus" : status_lulus
 }
 
-# TAMPILKAN HASIL
-
-print("\n===== RAPOR MAHASISWA =====")
-for key, value in rapor.items():
-    print(f"{key.replace('_', ' ').capitalize()}: {value}")
-print("============================")
+print(ringkasan)
